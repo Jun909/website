4bdb522504b8a8929bb7bc986dbc1c2ce6c56f2c
Jun909 <junsiang97@hotmail.com>
2024-11-10 11:04:14 +0100

    added chinese translation

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import UserInfo
+from django.utils.translation import gettext as _
 
 class UserInfoForm(forms.ModelForm):
     class Meta:
@@ -7,3 +8,6 @@ class UserInfoForm(forms.ModelForm):
         model = UserInfo
         fields = ["name", "highest_education", "gender", "major", "university", "experience_abroad", "target_working_place",
                   "type_of_job", "job_position_wanted", "email", "time_of_graduation", "birthdate", "political_info", "highest_title_achieved", "resume" ]
+        labels = {
+            'name' : '姓名'
+        }
--- mysite/settings.py
+++ mysite/settings.py
@@ -118,6 +118,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'zh-hans'
+# USE_I18N = True
+# USE_L10N = True
+# USE_TZ = True
 
 TIME_ZONE = 'Europe/Berlin'
 
